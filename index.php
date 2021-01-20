a00234bbda403a5d38b4adab5bfc0fcba9370700
AlexAloi <77560953+AlexAloi@users.noreply.github.com>
2021-01-20 15:23:16 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,52 @@
-<html>
-	<body>
-		<h1>Hello, CC_A2!</h1>
-	</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generic Games Site</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h3>Whack</h3>
+        Log In
+    </header>
+    <h2>New Games</h2>
+    <hr>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Icon</th>
+        </tr>
+        <tr>
+            <td>Game 1</td>
+            <td>icon.png</td>
+        </tr>
+        <tr>
+            <td>Game 2</td>
+            <td>icon.png</td>
+        </tr>
+    </table>
+    <a><-</a>  1/1  <a>-></a>
+    <hr>
+    <br />
+    <h2>Hot Games</h2>
+    <hr>
+    <table>
+    </tr>
+    <tr>
+        <td>Game 1</td>
+        <td>icon.png</td>
+    </tr>
+    <tr>
+        <td>Game 2</td>
+        <td>icon.png</td>
+    </tr>
+    </table>
+    <hr>
+
+    <footer>
+        <h4>whack</h4>
+    </footer>
+</body>
 </html>
