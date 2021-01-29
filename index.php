ad9aa8d061c11c60257d6085d68fe4f945d6dc45
AlexAloi <77560953+AlexAloi@users.noreply.github.com>
2021-01-30 00:20:10 +1100

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,18 @@ require 'vendor/autoload.php';
 use Aws\DynamoDb\Exception\DynamoDbException;
    
 @$jwt = $_GET["id_token"];
+    
+echo "fuck"
 
 if (isset($jwt)){
     #One liner decoding JWT token taken from
     #
     #development, C., Maintenance, S. and Tokens), P., 2021. PHP One-Liner To Decode JWT (JSON Web Tokens) | 2019. [online] 
     #Convertica eCommerce & membership website development. Available at: <https://www.converticacommerce.com/support-maintenance/security/php-one-liner-decode-jwt-json-web-tokens/> [Accessed 29 January 2021].
-    $jwt = (json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $jwt)[1])))));
+    print_r(json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $jwt)[1])))));
     echo $jwt;
+    echo "<br />";
+    echo $_SERVER["SCRIPT_URI"];
 }
 
 
